a73860fdf9ffd62e74a53ef0c11c23ef4440c435
leonardo-daryanto <103314397+leonardo-daryanto@users.noreply.github.com>
2022-04-30 09:51:07 +0700

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,17 @@
+//% weight=100 color=#C0392B icon=T
 
+namespace turtlePatern {
+    /**
+     *  start turtle patern
+     */
+    //% block
+    export function startTurtlePatern(): void {
+        turtle.setPositionCartesian(-80, -60)
+        forever(function () {
+            turtle.moveTurtleDirection(TurtleDirection.Forward, 25)
+            turtle.turnTurtleDirectionByDegrees(TurtleTurnDirection.Left, 90)
+            turtle.moveTurtleDirection(TurtleDirection.Forward, 25)
+            turtle.turnTurtleDirectionByDegrees(TurtleTurnDirection.Right, 90)
+        })
+    }
+}
